673b4c41c552b24eccd3508b2b5f10a3fa0d3875
JeremyGerm <jeremyrwiening@gmail.com>
2024-02-21 19:43:25 -0500

    Start of repo

---
+++ ASCIIart.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ASCIIart {
+    
+    public static void main(String[] args){
+
+         System.out.println("Which do you like more, cats or dogs?");
+
+         Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+    }
+}
